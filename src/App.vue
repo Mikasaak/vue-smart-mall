88a9c85a3ac462d750179c316cf19fa9bb298f01
ayaka <1474691736@qq.com>
2023-09-18 20:53:06 +0800

    添加Vant配置

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
   <div id="app">
+    <van-tabbar>
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
+
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      active: 0
+    }
+  }
+}
 </script>
 
 <style lang="less">
---
+++ src/utils/vant-ui.js
@@ -0,0 +1,7 @@
+import { Button, Icon, Tabbar, TabbarItem } from 'vant'
+import Vue from 'vue'
+
+Vue.use(Button)
+Vue.use(Icon)
+Vue.use(Tabbar)
+Vue.use(TabbarItem)
